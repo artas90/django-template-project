41852ede9b7f77f26e4d5fbe5b52f0e7174da586
Andrey Sokolov <falker@gala.net>
2014-05-20 23:09:31 +0300

    Add example of structure of js file

---
+++ source/projectname/main/static/main/pages/home_page/js/home_page.js
@@ -0,0 +1,18 @@
+ProjectName = window.ProjectName || {};
+ProjectName.main = ProjectName.main || {};
+
+
+ProjectName.main.page = {
+    init: function () {
+        this.initSomething();
+    },
+
+    initSomething: function () {
+
+    }
+};
+
+
+$(function () {
+    ProjectName.main.page.init();
+});
