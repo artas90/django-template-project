0b19b3c5bf582a648f019f326c28f47380166d35
Andrey Sokolov <a.sokolov@venista.com>
2015-01-04 19:38:52 +0200

    Add support for locale-specific date formats

--- conf/settings/common.py
+++ conf/settings/common.py
@@ -55,6 +55,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+FORMAT_MODULE_PATH = 'conf.formats'
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = root('var', 'media')
@@ -109,16 +111,22 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-# TEMPLATE_CONTEXT_PROCESSORS = (
-#     'django.contrib.auth.context_processors.auth',
-#     'django.core.context_processors.debug',
-#     'django.core.context_processors.i18n',
-#     'django.core.context_processors.media',
-#     'django.core.context_processors.static',
-#     'django.core.context_processors.tz',
-#     'django.contrib.messages.context_processors.messages',
-#     'django.core.context_processors.request',
-# )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    # Default context processors
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+
+    # Additional context processors
+    'django.core.context_processors.request',
+
+    # Our context processors
+    'projectname.main.context_processors.add_formats',
+)
 
 ROOT_URLCONF = 'source.urls'
 
